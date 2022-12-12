fda325133e16390ca8b342b5304a49d85183ed38
devraajmadhuri@gmail.com <devraajmadhuri@gmail.com>
2022-12-12 15:15:41 +0530

    Added:create binary serach tree using size

--- src/com/bridgelabz/bst/BinarySearchMain.java
+++ src/com/bridgelabz/bst/BinarySearchMain.java
@@ -1,5 +1,4 @@
 package com.bridgelabz.bst;
-
 public class BinarySearchMain {
     /**
      *  Create a Binary Search Tree by adding 56 and then adding 30 & 70.
@@ -10,7 +9,6 @@ public class BinarySearchMain {
      *3. Printing the elements
      */
     public static void main(String[] args) {
-
         /**
          * 1. Creating an object of function class
          */
@@ -19,18 +17,35 @@ public class BinarySearchMain {
          * 2.creating root node as null
          */
         INode root = null;
-
+        /**
+         * creating object of node class to get size of tree
+         */
+        INode size=new INode();
         /**
          * 3.Adding sub-nodes to the Binary Tree
          */
         root = link.insert(root, 56);
         root = link.insert(root, 30);
         root = link.insert(root, 70);
+        root = link.insert(root, 22);
+        root = link.insert(root, 40);
+        root = link.insert(root, 60);
+        root = link.insert(root, 95);
+        root = link.insert(root, 11);
+        root = link.insert(root, 65);
+        root = link.insert(root, 3);
+        root = link.insert(root, 16);
+        root = link.insert(root, 63);
+        root = link.insert(root, 67);
 
         /**
-         * 4.Printing the tree
+         * 5.Printing the tree
          */
         link.printBinarySearchTree(root);
-
+        /*
+         * 6. Getting Size of the Tree
+         */
+        int x = size.size(root);
+        System.out.println("\nSize of Tree: "+x);
     }
 }
